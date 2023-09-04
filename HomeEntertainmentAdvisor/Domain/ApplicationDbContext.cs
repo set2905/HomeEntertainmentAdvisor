81e9756408ae16bd096b8102666c6069cc2ef6c2
set2905 <72752613+set2905@users.noreply.github.com>
2023-09-04 22:42:26 +0400

    Like button component

--- HomeEntertainmentAdvisor/Domain/ApplicationDbContext.cs
+++ HomeEntertainmentAdvisor/Domain/ApplicationDbContext.cs
@@ -12,7 +12,6 @@ namespace HomeEntertainmentAdvisor.Data
             //Database.Migrate();
         }
 
-
         public DbSet<Comment> Comments { get; set; }
         public DbSet<ReviewLike> ReviewLikes { get; set; }
         public DbSet<MediaPiece> MediaPieces { get; set; }
@@ -25,33 +24,6 @@ namespace HomeEntertainmentAdvisor.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-
-            //builder.Entity<MediaPiece>().HasData(
-            // new MediaPiece()
-            // {
-            //     Id=new Guid("706C2E99-6F6C-4472-81A5-43C56E11637C"),
-            //     Name="TestMediaPiece"
-
-            // }
-            //);
-
-            //builder.Entity<Rating>().HasData(
-            // new Rating()
-            // {
-            //     Id=new Guid("bbd5efd3-bd3b-4bc9-8474-a6820a313483"),
-            //     MediaPieceId=new Guid("706C2E99-6F6C-4472-81A5-43C56E11637C")
-
-            // }
-            //);
-            //builder.Entity<Review>().HasData(
-            // new Review()
-            // {
-            //     Id=new Guid("ddd4edd3-bd3b-4bc9-8474-a6820a313483"),
-            //     RatingId=new Guid("bbd5efd3-bd3b-4bc9-8474-a6820a313483"),
-            //     Content="test TestSearch 1234",
-            //     Name="Test Review"
-            // }
-            //);
         }
     }
 }
